60c8107152cc544abef2965d8422876aae7bc593
alex-apf <alex.apfata.ing@gmail.com>
2025-07-11 18:17:47 -0500

    SEOsdsdsd

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-// next.config.mjs  (o .ts/.js)
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'es'],   // idiomas disponibles
-    defaultLocale: 'en',     // el que sirve cuando visitan “/”
-    localeDetection: false   // ⬅️  evita que Next te mande a /es
-  },
+
 };
 
 export default nextConfig;

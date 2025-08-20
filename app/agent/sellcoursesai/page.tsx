ce509248bea87867ca3f37a672fb219cb8fad3d1
alex-apf <alex.apfata.ing@gmail.com>
2025-08-20 14:40:26 -0500

    vende cursos ai

---
+++ app/agent/sellcoursesai/page.tsx
@@ -0,0 +1,18 @@
+
+import FlyerIALanding from "@/components/flyerIaLanding/FlyerIALanding";
+import { PixelGeneralMeta } from "@/components/pixel/pixelGeneralMeta/PixelGeneralMeta";
+import VendeCursosAI from "@/components/vendecursosai/VendeCursosAI";
+import React, { Suspense } from "react";
+
+export default function Page() {
+  return (
+    <>
+      <main>
+        <VendeCursosAI />
+      </main>
+      <Suspense fallback={null}>
+        <PixelGeneralMeta pixelId="1021962520134015" />
+      </Suspense>
+    </>
+  );
+}

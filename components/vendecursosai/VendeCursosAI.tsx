4ac9a79d78002b1f7f43360295bd4ed162cfa307
alex-apf <alex.apfata.ing@gmail.com>
2025-08-23 09:07:42 -0500

    ultimos cambiose

--- components/vendecursosai/VendeCursosAI.tsx
+++ components/vendecursosai/VendeCursosAI.tsx
@@ -968,14 +968,14 @@ export default function VendeCursosAI() {
 
         {/* Fixed CTA Buttons */}
         <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50">
-          <button
+          <a
             type="button"
             onClick={() => window.open(linkPagoHotmart, "_blank")}
             className="bg-gradient-to-r from-orange-500 to-pink-500 hover:from-orange-600 hover:to-pink-600 text-white font-bold px-8 py-4 rounded-full shadow-2xl animate-pulse"
           >
             Comprar por <CountryPriceText prices={priceMap} />
             <span className="text-sm align-top">/mes</span>
-          </button>
+          </a>
         </div>
       </div>
     </>
